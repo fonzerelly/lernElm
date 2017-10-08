8841fc2e199650fc65428863a761bad5c766401d
cs8433 <christian.hoerauf@consorsbank.de>
2017-10-08 14:42:34 +0200

    Dice rendered as svg

--- Dice.elm
+++ Dice.elm
@@ -1,6 +1,9 @@
 import Html exposing (..)
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (src)
 import Random
+import Svg exposing (svg, circle)
+import Svg.Attributes exposing (r, cx, cy, x, y, version, viewBox, width, height)
 
 main = Html.program {
     init = init,
@@ -17,11 +20,57 @@ init: (Model, Cmd Msg)
 init = 
     (Model 1, Cmd.none)
 
+dice: Int -> Html Msg
+dice num =
+    case num of
+        1 ->
+            svg [ version "1.1", width "60", height "60"] [
+                circle [ cx "30", cy "30", r "5" ] []
+            ]
+        2 ->
+            svg [ version "1.1", width "60", height "60"] [
+                circle [ cx "10", cy "10", r "5" ] [],
+                circle [ cx "50", cy "50", r "5"] []
+            ]
+        3 ->
+            svg [ version "1.1", width "60", height "60"] [
+                circle [ cx "10", cy "10", r "5" ] [],
+                circle [ cx "30", cy "30", r "5" ] [],
+                circle [ cx "50", cy "50", r "5"] []
+            ]
+        4 ->
+            svg [ version "1.1", width "60", height "60"] [
+                circle [ cx "10", cy "10", r "5" ] [],
+                circle [ cx "10", cy "50", r "5" ] [],
+                circle [ cx "50", cy "10", r "5" ] [],
+                circle [ cx "50", cy "50", r "5"] []
+            ]
+        5 ->
+            svg [ version "1.1", width "60", height "60"] [
+                circle [ cx "10", cy "10", r "5" ] [],
+                circle [ cx "10", cy "50", r "5" ] [],
+                circle [ cx "30", cy "30", r "5" ] [],
+                circle [ cx "50", cy "10", r "5" ] [],
+                circle [ cx "50", cy "50", r "5"] []
+            ]
+        6 ->
+            svg [ version "1.1", width "60", height "60"] [
+                circle [ cx "10", cy "10", r "5" ] [],
+                circle [ cx "10", cy "50", r "5" ] [],
+                circle [ cx "10", cy "30", r "5" ] [],
+                circle [ cx "50", cy "30", r "5" ] [],
+                circle [ cx "50", cy "10", r "5" ] [],
+                circle [ cx "50", cy "50", r "5"] []
+            ]
+        _ -> text <| toString num ++ " is not defined as dice"
+            
+
 view: Model -> Html Msg
 view model = 
     div [] [
         h1 [] [ text "Würfel" ],
-        h2 [] [ text <| toString model.dieFace ],
+        dice model.dieFace,
+        br [] [],  
         button [onClick Roll] [text "Roll"]
     ]
 
