d841a44b6c774d570db2c0949b5a107458601491
cs8433 <christian.hoerauf@consorsbank.de>
2017-10-15 23:21:17 +0200

    Hours and minutes added

--- Clock.elm
+++ Clock.elm
@@ -2,6 +2,7 @@
 
 
 import Html exposing (..)
+import Html.Events exposing (onClick)
 import String exposing (concat)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
@@ -17,25 +18,31 @@ main =
     }
 
 
-type alias Model = Time
+type alias Model =
+    {   time: Time
+    ,   pause: Bool
+    }
 
 
 type Msg
     = Tick Time
+    | Pause
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         Tick time ->
-            (time, Cmd.none)
+            ({model | time = time}, Cmd.none)
+        Pause ->
+            ({model | pause = not model.pause}, Cmd.none)
 
 
 view : Model -> Html Msg
 view model =
     let
         angle = 
-            turns (Time.inMinutes model)
+            turns (Time.inMinutes model.time) - (pi/2)
         handX = 
             toString (50 + 40 * cos angle)
         handY = 
@@ -44,11 +51,41 @@ view model =
         div []
             [
                 svg [ viewBox "0 0 100 100", width "300px"]
-                [circle [ cx "50", cy "50",  r "45", fill "#0B79CE"] []
-                , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963"] []  
+                [   circle [ cx "50", cy "50",  r "45", fill "#0B79CE"] []
+                ,   hand Second model.time
+                ,   hand Minute model.time
+                ,   hand Hour model.time
                 ]
-            ,    Html.text <| currentTime model
+            ,   button [ onClick Pause ] [Html.text "Pause"]
+            ,   Html.text <| currentTime model.time
             ]
+
+type HandType = Second | Minute | Hour
+
+hand : HandType -> Time -> Html Msg
+hand handType time =
+    let
+        inFn = case handType of
+            Second -> Time.inMinutes
+            Minute -> Time.inHours
+            Hour -> \t -> ((/) 24) <| Time.inHours <| t
+        angle =
+            case handType of
+                Hour -> turns (inFn time) - (4*pi/6)
+                _ -> turns (inFn time) - (pi/2)
+
+        handX =
+            case handType of
+            Hour -> toString (50 + 30 * cos angle)
+            _ -> toString (50 + 40 * cos angle)
+        handY =
+            case handType of
+            Hour -> toString ( 50 + 30 * sin angle)
+            _ -> toString ( 50 + 40 * sin angle)
+    in
+        case handType of
+            Hour -> line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963", strokeWidth "3"] []
+            _ -> line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963"] []
 
 timeAsInt: (Time -> Float) -> Time -> Int
 timeAsInt inFn time = round <| inFn time
@@ -57,16 +94,16 @@ secondsAsInt = timeAsInt inSeconds
 minutesAsInt = timeAsInt inMinutes
 hoursAsInt = timeAsInt inHours
 
-currentSeconds : Model -> Int
+currentSeconds : Time -> Int
 currentSeconds time = ((secondsAsInt time)% 60)
 
-currentMinutes : Model -> Int
+currentMinutes : Time -> Int
 currentMinutes time = ((secondsAsInt time) % 3600) // 60
 
-currentHours : Model -> Int
+currentHours : Time -> Int
 currentHours time = (((secondsAsInt time) % 86400 ) // 3600 + 2)
 
-currentTime: Model -> String
+currentTime: Time -> String
 currentTime time = concat [ toString <| currentHours time
                           , ":"
                           , toString <| currentMinutes time
@@ -76,9 +113,9 @@ currentTime time = concat [ toString <| currentHours time
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Time.every second Tick
+    if model.pause then Time.every second Tick else Sub.none
 
 
 init : (Model, Cmd Msg)
 init = 
-    (0, Cmd.none)
+    ({time = 0, pause = True}, Cmd.none)
